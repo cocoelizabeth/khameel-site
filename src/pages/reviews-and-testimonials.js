4bb473483fa2d9e2a8a41a954212fa28cd3d7655
Coco K <coco@mac.mynetworksettings.com>
2026-03-17 17:22:29 -0400

    Add Reviews and Testimonials page component

---
+++ src/pages/reviews-and-testimonials.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ReviewsAndTestimonials = () => (
+  <main>
+    <h1>Reviews and Testimonials</h1>
+    <p>Content goes here.</p>
+  </main>
+);
+
+export default ReviewsAndTestimonials;
